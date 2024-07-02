ecc8a78c76eca05e4a5b8e6aa2e244eda5535122
朱世杰 <zhushijie@xunfeng.cn>
2024-07-02 18:23:51 +0800

    docs: update n queen

--- algorithm/n-queen.js
+++ algorithm/n-queen.js
@@ -33,9 +33,38 @@ function solveNQueens(n) {
     return ans;
 }
 
+function solveNQueens1(n) {
+    const ans = [];
+    const path = Array(n).fill(0)
+    const onPath = Array(n).fill(false)
+    const diag1 = Array(2 * n - 1).fill(false)
+    const diag2 = Array(2 * n - 1).fill(false)
+
+    function dfs(r) {
+        if (r === n) {
+            ans.push(buildSolution(path, n))
+            return
+        }
+
+        for (let c = 0; c < n; c++) {
+            const rc = r -c + n - 1
+            if (!onPath[c] && !diag1[r + c] && !diag2[rc]) {
+                path[r] = c
+                onPath[c] = diag1[r + c] = diag2[rc] = true
+                dfs(r + 1)
+                onPath[c] = diag1[r + c] = diag2[rc] = false
+            }
+        }
+    }
+
+    dfs(0)
+
+    return ans;
+}
+
 function buildSolution(path, n) {
     return path.map(c => '.'.repeat(c) + 'Q' + '.'.repeat(n - c - 1))
 }
 
-console.log(solveNQueens(4));
+console.log(solveNQueens1(4));
 // console.log(buildSolution([1, 1, 1, 1], 4))
