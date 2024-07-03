48994743b1914444bc6b498fe6e5d127195f11e8
朱世杰 <zhushijie@xunfeng.cn>
2024-07-03 09:29:49 +0800

    docs: update n queen

--- algorithm/n-queen.js
+++ algorithm/n-queen.js
@@ -2,6 +2,12 @@ function solveNQueens(n) {
     const ans = [];
     const path = new Array(n).fill(0)
 
+    // 这里是判断对角线上是否有冲突的皇后
+    // r, c 分别表示行坐标和列坐标
+    // 因为是从上往下遍历，所以我们只需要看
+    // 左上和右上的对角线
+    // 对于右上对角线，r 和 c 的和是一个固定值
+    // 对于左上对角线，r 和 c 的差是一个固定值
     function isValid(r, c) {
         for (let R = 0; R < r; R++) {
             let C = path[R]
@@ -12,9 +18,13 @@ function solveNQueens(n) {
         return true
     }
 
+    // 对了，isValid 为什么不需要判断列是否有冲突呢
+    // 因为 dfs 函数中做了这个限制
+    //  dfs 函数的 s 参数就是表示剩余可以处理的列
+    // 每次查找到一个有效位置后，都会将当前列从 s 集合中排除
     function dfs(r, s) {
         if (r === n) {
-            ans.push(buildSolution(path, n))
+            ans.push(path.map(c => '.'.repeat(c) + 'Q' + '.'.repeat(n - c - 1)))
             return
         }
 
@@ -33,10 +43,23 @@ function solveNQueens(n) {
     return ans;
 }
 
+/**
+ * 上面的解法有点可以优化：
+ * 1. 每次都需要新建一个集合
+ * 2. 判断 isValid 判断比较复杂
+ * 
+ * 我们可以用一个布尔数组来存储列和对角线的放置结果，
+ * 优化可用位置的判断
+ */
+
 function solveNQueens1(n) {
     const ans = [];
     const path = Array(n).fill(0)
     const onPath = Array(n).fill(false)
+
+    // 对角线：
+    // 左上差值固定 r - c，范围是 -n ～ n，所以长度为 2 * n - 1
+    // 右上和固定 r + c，范围是 0 ～ 2n，所以长度也为 2 * n - 1
     const diag1 = Array(2 * n - 1).fill(false)
     const diag2 = Array(2 * n - 1).fill(false)
 
@@ -47,6 +70,8 @@ function solveNQueens1(n) {
         }
 
         for (let c = 0; c < n; c++) {
+            // 左上对角差值固定 r - c，范围是 -n ～ n，所以长度为 2 * n - 1
+            // 为了防止出现负数，所以要加上 n - 1
             const rc = r -c + n - 1
             if (!onPath[c] && !diag1[r + c] && !diag2[rc]) {
                 path[r] = c
