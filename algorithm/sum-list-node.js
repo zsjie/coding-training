32503a5df5565306d86f68756b226e93db46d773
朱世杰 <zhushijie@xunfeng.cn>
2025-06-27 08:54:46 +0800

    feat: update add two numbers

--- algorithm/sum-list-node.js
+++ algorithm/sum-list-node.js
@@ -1,14 +1,10 @@
 /**
  * Definition for singly-linked list.
-
-
-
-
-
-* }
-
-
-*/
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
 
 function ListNode(val, next) {
     this.val = (val===undefined ? 0 : val)
@@ -16,28 +12,27 @@ function ListNode(val, next) {
 }
 
 var addTwoNumbers = function(l1, l2) {
-    let head = null, tail = null
-    let last = 0
-    while (l1 || l2) {
-        const tmp = (l1?.val || 0) + (l2?.val || 0) + last
+    const dummy = new ListNode(0)
+    let cur = dummy
+    let carry = 0
 
-        if (!head) {
-            head = tail = new ListNode(tmp % 10)
-        } else {
-            tail = tail.next = new ListNode(tmp % 10)
-        }
-
-        last = Math.floor(tmp / 10)
-
-        if (l1) {
-            l1 = l1.next
-        }
-        if (l2) {
-            l2 = l2.next
-        }
+    while (l1 || l2 || carry) {
+        // 获取当前节点的值，如果节点为空则为0
+        const val1 = l1 ? l1.val : 0
+        const val2 = l2 ? l2.val : 0
+        
+        // 计算和与进位
+        const sum = val1 + val2 + carry
+        carry = Math.floor(sum / 10)
+        
+        // 创建新节点
+        cur.next = new ListNode(sum % 10)
+        cur = cur.next
+        
+        // 移动到下一个节点
+        l1 = l1 ? l1.next : null
+        l2 = l2 ? l2.next : null
     }
 
-    if (last != 0) tail.next = new ListNode(1)
-
-    return head
+    return dummy.next
 };
